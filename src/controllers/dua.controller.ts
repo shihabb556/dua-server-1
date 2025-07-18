d7e8657c29c7ba41dd9551541e34da37a1efc4f6
shihabb556 <shihabaly556@gmail.com>
2025-07-18 07:01:21 +0600

    Add dua controller with endpoints for retrieving all duas and a specific dua by ID
    - Implemented `getDuas` to fetch all duas from the database.
    - Implemented `getDuaById` to fetch a specific dua based on the provided ID.
    - Added error handling for both endpoints to ensure proper response in case of failures.

---
+++ src/controllers/dua.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import db from "../db/config";
+
+
+//get all duas
+// GET /api/duas
+export const getDuas = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const duas = await db.prepare("SELECT * FROM dua").all();
+        res.status(200).json(duas);
+    } catch (error) {
+        next(error);
+    }
+};
+
+//get dua by id
+// GET /api/duas/:id
+export const getDuaById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+        const dua = await db.prepare("SELECT * FROM dua WHERE id = ?").all(id);
+        res.status(200).json(dua);
+    } catch (error) {
+        next(error);
+    }
+};
