647010fe68dfd5eeb96c9da730bacc1a9f3c3429
shihabb556 <shihabaly556@gmail.com>
2025-07-18 07:39:58 +0600

    Add category and dua routes with corresponding endpoints
    - Introduced `category.routes.ts` to handle requests for categories and subcategories.
    - Added `dua.routes.ts` to manage requests for duas, including retrieval by category and subcategory IDs.
    - Enabled `esModuleInterop` in `tsconfig.json` for improved module compatibility.

---
+++ src/routes/category.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getCategories, getSubcategoriesByCategoryId } from "@/controllers/category.controller";
+
+const router = Router();
+
+router.get("/", getCategories);
+router.get("/:id", getSubcategoriesByCategoryId);
---
+++ src/routes/dua.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getDuas, getDuaById, getDuasByCategoryId, getDuasBySubcategoryId } from "@/controllers/dua.controller";
+
+const router = Router();
+
+router.get("/", getDuas);
+router.get("/:id", getDuaById);
+router.get("/category/:id", getDuasByCategoryId);
+router.get("/subcategory/:id", getDuasBySubcategoryId);
+
+export default router;
